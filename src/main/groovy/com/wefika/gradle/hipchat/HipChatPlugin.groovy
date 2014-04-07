5a291aa89f3114de5fdf092fd932b0a418fc7784
Blaz Solar <solarblaz@gmail.com>
2014-04-07 16:51:48 +0200

    Addding copyright

--- src/main/groovy/com/wefika/gradle/hipchat/HipChatPlugin.groovy
+++ src/main/groovy/com/wefika/gradle/hipchat/HipChatPlugin.groovy
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2014 Blaž Šolar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wefika.gradle.hipchat
 import com.wefika.gradle.hipchat.dsl.HipChatPluginExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
 /**
+ * HipChat plugin
+ *
  * Created by blazsolar on 07/04/14.
  */
 class HipChatPlugin implements Plugin<Project> {
